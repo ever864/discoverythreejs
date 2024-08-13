d7323c1e29064c1bc607bbc83c68ae01d71069b0
Ever Francisco Ponce Lara <ever864@gmail.com>
2024-08-12 22:20:57 -0500

    feat: create simple box

--- src/main.js
+++ src/main.js
@@ -1,4 +1,44 @@
-import * as THREE from "three";
+import {
+  BoxBufferGeometry,
+  Color,
+  Mesh,
+  MeshBasicMaterial,
+  PerspectiveCamera,
+  Scene,
+  WebGLRenderer,
+} from "https://cdn.skypack.dev/three@0.132.2";
 
-import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
-import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
+console.log("Hello, World!");
+
+const container = document.querySelector("#scene-container");
+
+const scene = new Scene();
+
+scene.background = new Color("skyblue");
+
+const fov = 60;
+const aspect = container.clientWidth / container.clientHeight;
+const near = 0.1;
+const far = 1000;
+
+const camera = new PerspectiveCamera(fov, aspect, near, far);
+
+camera.position.set(0, 0, 10);
+
+const geometry = new BoxBufferGeometry(2, 2, 2);
+
+const material = new MeshBasicMaterial();
+
+const cube = new Mesh(geometry, material);
+
+scene.add(cube);
+
+const renderer = new WebGLRenderer();
+
+renderer.setSize(container.clientWidth, container.clientHeight);
+
+renderer.setPixelRatio(window.devicePixelRatio);
+
+container.append(renderer.domElement);
+
+renderer.render(scene, camera);
